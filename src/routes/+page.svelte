c626f737177fbbfdd8d42d69c5973ff8fb53869f
Colton <zxcolt@gmail.com>
2023-03-28 16:26:10 -0500

    stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
+<script>
+	import Navbar from './navbar.svelte';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<Navbar />
+
+<style>
+</style>
